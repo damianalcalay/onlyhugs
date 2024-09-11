cfad786b11a8d2c863a34e87c4439c100860d7e3
unknown <damianalcalayy@gmail.com>
2024-09-11 04:58:09 +0100

    asda

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Statistics from "@/components/ui/statistics/Statistics";
-import Contact from "../components/ui/contact/Contact";
-import Footer from "../components/ui/footer/Footer";
-import Hero from "../components/ui/hero/Hero";
-import Features from "@/components/ui/features/Features";
-import Navbar from "@/components/ui/navbar/Navbar";
+import Statistics from "@/components/statistics/Statistics";
+import Contact from "../components/contact/Contact";
+import Footer from "../components/footer/Footer";
+import Hero from "../components/hero/Hero";
+import Features from "@/components/features/Features";
+import Navbar from "@/components/navbar/Navbar";
 
 export default function Home() {
   return (
