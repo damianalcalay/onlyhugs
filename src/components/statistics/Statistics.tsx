126108b7e7b7e18d9a094fe5e5d4e828da097641
unknown <damianalcalayy@gmail.com>
2024-09-15 17:22:46 +0100

    fixed responsive improved footer

--- src/components/statistics/Statistics.tsx
+++ src/components/statistics/Statistics.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 
 // Definimos la animación para todo el bloque
